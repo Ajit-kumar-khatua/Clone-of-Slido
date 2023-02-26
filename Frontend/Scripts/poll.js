8a6b9dd6fdc43bdbed298d3bbfe0cfecf9d0fb49
unknown <ajitkumarkhatua286@gmail.com>
2023-02-26 14:37:08 +0530

    completed

--- Frontend/Scripts/poll.js
+++ Frontend/Scripts/poll.js
@@ -3,6 +3,8 @@
 // const room= urlParams.get("room")
 
 let room=localStorage.getItem("roomno")
+let baseUrl="https://polleasy.onrender.com"
+let token= localStorage.getItem("token")
 
 let responseForm=document.getElementById("response-form")
 
@@ -26,3 +28,17 @@ responseForm.addEventListener("submit",(event)=>{
     let res=document.getElementById("res").value;
     socket.emit("response",res)
 })
+
+async function getpoll(){
+    try {
+        let res=await fetch(`${baseUrl}/polls/${room}`,{
+            method: "GET"
+        })
+        let data=await res.json()
+        console.log(data[0].polls)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+getpoll()
